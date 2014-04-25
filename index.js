6a00ea51338058dbac15910ef42d0e4bb84c520a
Karl-Johan Sjogren <karl-johan.sjogren@openlibrarysolutions.com>
2014-04-25 17:41:37 +0200

    Added a text-only visualization as well using masonry.js

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
-/* global require, __dirname, process */
+/* global require, __dirname, process, setInterval */
 /*jshint indent:2 */
 require('newrelic');
 var express = require('express'),
@@ -48,41 +48,45 @@ app.post('/recieve', function (req, res) {
 }
 
 */
-/*
-var randomTitles = ['Ondskan', 'Bröderna lejonhjärta', 'En sak å en annan', 'Min bok om mig', 'Isprinsessan', 'En tandläkares mardröm', 'Hur man skriver en listetta', 'Bläckfiskresan'];
-var randomCoordinates = [
-  [63.829768, 20.263596], // Umeå
-  [63.093516, 21.676025], // Vasa
-  [57.718819, 12.944641], // Borås
-  [63.173574, 14.660568]  // Östersund
-];
 
-setInterval(function() {
-  randomCoordinates = shuffle(randomCoordinates);
-  randomTitles = shuffle(randomTitles);
-  var coords = randomCoordinates[0];
-  var obj = {
-    'keywords': randomTitles[0],
-    'lat': coords[0],
-    'lon': coords[1]
+if(process.env.NODE_ENV !== 'production') {
+  var shuffle = function(array) {
+    var m = array.length, t, i;
+
+    // While there remain elements to shuffle…
+    while (m) {
+
+      // Pick a remaining element…
+      i = Math.floor(Math.random() * m--);
+
+      // And swap it with the current element.
+      t = array[m];
+      array[m] = array[i];
+      array[i] = t;
+    }
+
+    return array;
   };
-  io.sockets.emit('search', obj);
-}, 300);
+  
+  var randomTitles = ['Ondskan', 'Bröderna lejonhjärta', 'En sak å en annan', 'Min bok om mig', 'Isprinsessan', 'En tandläkares mardröm', 'Hur man skriver en listetta', 'Bläckfiskresan'];
+  var randomCoordinates = [
+    [63.829768, 20.263596], // Umeå
+    [63.093516, 21.676025], // Vasa
+    [57.718819, 12.944641], // Borås
+    [63.173574, 14.660568]  // Östersund
+  ];
 
-function shuffle(array) {
-  var m = array.length, t, i;
+  setInterval(function() {
+    randomCoordinates = shuffle(randomCoordinates);
+    randomTitles = shuffle(randomTitles);
+    var coords = randomCoordinates[0];
+    var obj = {
+      'keywords': randomTitles[0],
+      'lat': coords[0],
+      'lon': coords[1]
+    };
+    io.sockets.emit('search', obj);
+  }, 300);
 
-  // While there remain elements to shuffle…
-  while (m) {
-
-    // Pick a remaining element…
-    i = Math.floor(Math.random() * m--);
-
-    // And swap it with the current element.
-    t = array[m];
-    array[m] = array[i];
-    array[i] = t;
-  }
-
-  return array;
-}*/
+  
+}
