7470a5e2717ae0fdea526ed47a82d6b1ba9d75f0
Karl-Johan Sjögren <karl.sjogren@gmail.com>
2014-06-22 19:04:03 +0200

    Prepared for socket.io connetions from provider

--- routes/statistics.js
+++ routes/statistics.js
@@ -7,6 +7,75 @@ var promiseFor = require('../helpers/promise-for');
 var Q = require('q');
 
 module.exports = function(app, io) {
+  function saveStats(item, callback) {
+    statistics.save(item).then(function() {
+      if(!!item.type) {
+        var type = item.type.toLowerCase();
+        if(type.indexOf('search') !== -1) {
+          var search = {
+            'keywords': item.freeText,
+            'lat': client.latitude,
+            'lon': client.longitude
+          };
+
+          if(!!search && search.keywords.length > 0)
+            io.sockets.emit('search', search);  // No need to emit empty keywords
+
+        } else if(type === 'performance') {
+          var perf = {
+            clientKey: item.clientKey,
+            cpuUsage: item.cpuUsage,
+            availableMemory: item.availableMemory,
+            totalMemory: item.totalMemory,
+            memoryUsage: item.memoryUsage
+          };
+
+          io.sockets.emit(type, perf);
+
+        } else if(type === 'dataminerstats') {
+          var miner = {
+            clientKey: item.clientKey,
+            minerName: item.minerName,
+            catalogName: item.catalogName,
+            catalogId: item.catalogId,
+            scannedWorksPerMinute: item.scannedWorksPerMinute,
+            lastScannedId: item.lastScannedId              
+          };
+
+          io.sockets.emit(type, miner);
+
+        } else if(type === 'reindexstats') {
+          var reindex = {
+            totalProcessed : item.totalProcessed,
+            pageSize: item.pageSize,
+            lastId: item.lastId
+          };
+
+          io.sockets.emit(type, reindex);
+
+        }
+
+        if(!!callback))
+          callback();
+      }
+    });
+  };
+  
+  io.on('ola-connected', function(data) {
+  });
+  
+  io.on('ola-disconnected', function(data) {
+  });
+  
+  io.on('statistics', function(data) {
+    if (typeof myVar == 'string' || myVar instanceof String) {
+      console.log('A string was recieved as statistics via socket.io, trying to parse it.');
+      console.log(data);
+      data = JSON.parse(data);
+    }
+    saveStats(data);
+  });
+  
   app.put('/statistics/collect', function (req, res) {
     var body = req.body;
     if(!body) {
@@ -29,55 +98,7 @@ module.exports = function(app, io) {
           return done.reject('Invalid clientKey specified: ' + clientKey);
         }
 
-        statistics.save(item).then(function() {
-          if(!!item.type) {
-            var type = item.type.toLowerCase();
-            if(type.indexOf('search') !== -1) {
-              var search = {
-                'keywords': item.freeText,
-                'lat': client.latitude,
-                'lon': client.longitude
-              };
-
-              if(!!search && search.keywords.length > 0)
-                io.sockets.emit('search', search);  // No need to emit empty keywords
-              
-            } else if(type === 'performance') {
-              var perf = {
-                clientKey: item.clientKey,
-                cpuUsage: item.cpuUsage,
-                availableMemory: item.availableMemory,
-                totalMemory: item.totalMemory,
-                memoryUsage: item.memoryUsage
-              };
-
-              io.sockets.emit(type, perf);
-              
-            } else if(type === 'dataminerstats') {
-              var miner = {
-                clientKey: item.clientKey,
-                minerName: item.minerName,
-                catalogName: item.catalogName,
-                catalogId: item.catalogId,
-                scannedWorksPerMinute: item.scannedWorksPerMinute,
-                lastScannedId: item.lastScannedId              
-              };
-              
-              io.sockets.emit(type, miner);
-              
-            } else if(type === 'reindexstats') {
-              var reindex = {
-                totalProcessed : item.totalProcessed,
-                pageSize: item.pageSize,
-                lastId: item.lastId
-              };
-              
-              io.sockets.emit(type, reindex);
-              
-            }
-            done.resolve();
-          }
-        });
+        saveStats(item, function() { done.resolve(); });
       });
 
       return done.promise;
@@ -88,4 +109,6 @@ module.exports = function(app, io) {
       res.end();
     });
   });
+  
+  
 };
